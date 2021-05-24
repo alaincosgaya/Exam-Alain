9c4df3f1a699f6e07e0748b27aae08180038ddf3
1dam <1dam@HZ301190.TartangaLH.eus>
2021-05-24 09:21:49 +0200

    commit-javadoc

--- ControlVersiones/src/controlVersiones/Empleado.java
+++ ControlVersiones/src/controlVersiones/Empleado.java
@@ -1,11 +1,23 @@
 package controlVersiones;
 
+/**
+ * 
+ * @author Alain
+ * @version Version 1,2021
+ * @since 24/05/2021
+ * 
+ */
 public class Empleado {
 	private String nombre;
 	private String apellidos;
 	private double salario; 
 
-
+/**
+ * 
+ * @param nombre
+ * @param apellido
+ * @param salario
+ */
 public Empleado(String nombre, String apellido, double salario) {
 	this.nombre = nombre;
 	this.apellidos = apellido;
@@ -13,26 +25,55 @@ public Empleado(String nombre, String apellido, double salario) {
 }
 
 
-
+/**
+ * 
+ * @return nombre
+ */
 public String getNombre() {
 	return nombre;
 }
+/**
+ * 
+ * @param nombre
+ */
 public void setNombre(String nombre) {
 	this.nombre = nombre;
 }
+/**
+ * 
+ * @return apellidos
+ */
 public String getApellidos() {
 	return apellidos;
 }
+/**
+ * 
+ * @param apellidos
+ */
 public void setApellidos(String apellidos) {
 	this.apellidos = apellidos;
 }
+/**
+ * 
+ * @return salario
+ */
 public double getSalario() {
 	return salario;
 }
+/**
+ * 
+ * @param salario
+ * 
+ */
 public void setSalario(double salario) {
 	this.salario = salario;
 }
 
+/**
+ * 
+ * @param subida
+ * @apiNote salario + subida
+ */
 public void subirsalario (double subida) {
 	salario=salario+subida;
 }
